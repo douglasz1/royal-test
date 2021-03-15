fd68abe87cc07edf4211586ffdccef0e410d74ef
douglasz1 <douglas.santana.dev@gmail.com>
2021-03-14 22:25:25 -0300

    absolute route conditions

--- config/config.php
+++ config/config.php
@@ -1,7 +1,8 @@
 <?php
-#CAminhos absolutos
+#Caminhos absolutos
 $pastaInterna = "";
 define('DIRPAGE', "http://{$_SERVER['HTTP_HOST']}/{$pastaInterna}");
 
+(substr($_SERVER['DOCUMENT_ROOT'],-1) == '/') ?$barra = "":$barra = "/";
 
-define('DIRREQ', "{$_SERVER['DOCUMENT_ROOT']}{$pastaInterna}");
+define('DIRREQ', "{$_SERVER['DOCUMENT_ROOT']}($barra){$pastaInterna}");
